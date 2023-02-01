ede1597f6d3a5e80b612ecf03f9abd534a4fa47d
Christina P <reetagreen@users.noreply.github.com>
2023-02-01 17:58:36 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-player_choice="rock" #player's choice
-opp_choice="scissors" #opponent's choice
+import random #importing the random library
+
+#variables:
+something=2
+anything="Hello world"
+#a list:
+food = ["pizza","pasta","cannelloni","sushi"]
+dinner=random.choice(food)
+print (dinner)
+
+
+#a function:
+def choices():
+  choicesList=["rock","paper","scissors"]
+  player_choice=input("Enter a choice (rock-paper-scissors):") 
+  opponent_choice= random.choice(choicesList)
+  #dictionary of choices:
+  choicesDic={"Player":player_choice,"Opponent":opponent_choice}
+  return choicesDic
+
+def greeting():
+  return "Hi!"
+
+#calling a function:
+greeting()
+
+response=greeting() ## response will be a variable
+print(response) #prints out to the console
+
+choicesVar=choices()
+print(choicesVar)
+
+color="Red"
+#dictionary: key+value
+dict={"Name": "beau","colour":color}
+
+print(anything)
+print(something)
